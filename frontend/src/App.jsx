f2368eadaa42b963207151cf475b6d9f6f587e53
jamesdai05 <dai_guilong@hotmail.com>
2025-03-04 01:03:38 +0800

    created the basic UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css';
 import Footer from './components/Footer.jsx';
 import Header from './components/Header.jsx';
+import { Container } from 'react-bootstrap';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Container>
+        <main className='pt-3'>
+          <h2>Welcome to the platform!</h2>
+        </main>
+      </Container>
       <Footer />
     </div>
   );
