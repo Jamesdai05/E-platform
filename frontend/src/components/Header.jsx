f775a3ee5c5ecc4778bb9cbcd277b99066833e1f
jamesdai05 <dai_guilong@hotmail.com>
2025-03-04 00:50:16 +0800

    add the header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { Container, Nav, Navbar, Badge, NavDropdown } from "react-bootstrap";
+// import Container from "react-bootstrap/Container";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+
+const Header = () => {
+
+  const fsStyle = { color: "#fff" };
+
+  return (
+    <Navbar expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">
+          E-shop
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav> */}
+          <Nav className="ms-auto">
+            <Nav.Link href="/cart" style={fsStyle}>
+              Cart <FaShoppingCart className="ms-0.5" />
+            </Nav.Link>
+            <Nav.Link href="/login" style={fsStyle}>
+              User <FaUser className="ms-0.5" />
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+export default Header;
