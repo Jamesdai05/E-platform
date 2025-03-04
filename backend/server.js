75ef79d2ad7f4a673df4a6c99150564aadcd7d41
jamesdai05 <dai_guilong@hotmail.com>
2025-03-04 13:16:49 +0800

    add the data

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,13 @@ app.get("/",(req,res)=>{
   res.send("API is running...")
 })
 
+
+//get products
+app.get("/api/products",(req,res)=>{
+  res.json(products)
+})
+
+
 app.listen(port,()=>{
   console.log(`Server is listening on port ${port}.`);
 })
