9c7a097be2d8df2cd6d308cf296382b9b175c762
jamesdai05 <dai_guilong@hotmail.com>
2025-03-04 15:26:11 +0800

    add the user sample data

---
+++ backend/data/users.js
@@ -0,0 +1,31 @@
+
+const users= [
+  {
+    name:"Admin user",
+    email:"admin@email.com",
+    password:"abc123456",
+    isAdmin:true,
+  },
+
+  {
+    name:"John Green",
+    email:"jonh_green@email.com",
+    password:"abc123456",
+  },
+
+  {
+    name:"Jacky Chan",
+    email:"jacky_chan@email.com",
+    password:"abc123456",
+  },
+
+  {
+    name:"John Doe",
+    email:"john@email.com",
+    password:"a123456",
+  },
+
+]
+
+
+export default users;
