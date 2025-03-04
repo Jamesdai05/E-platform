273844bec622113812cb3036b8bfb676ba404642
jamesdai05 <dai_guilong@hotmail.com>
2025-03-04 13:21:22 +0800

    add the 2 routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // console.log("Hello world");
 import express from "express"
+import products from "./data/products.js"
 const app=express()
 
 const port =5002
@@ -12,6 +13,12 @@ app.get("/",(req,res)=>{
 //get products
 app.get("/api/products",(req,res)=>{
   res.json(products)
+})
+
+// specific product
+app.get("/api/products/:id",(req,res)=>{
+  const product=products.find(e=>e._id==req.params.id)
+  res.json(product)
 })
 
 
