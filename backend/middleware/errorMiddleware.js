eb98ca981b6322405881b38462cfb14df80f097c
jamesdai05 <dai_guilong@hotmail.com>
2025-03-04 17:51:01 +0800

    change error handler

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -12,29 +12,24 @@ const notFound = (req, res, next) => {
 
 
 const errorHandler=(err,req,res,next)=>{
-  try{
-    if(res.headersSent){
-      return next(err)
-    }
+  let statusCode=res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message || 'An unexpected error occurred';
 
-    let statusCode=res.statusCode === 200 ? 500 : res.statusCode;
-    let message = err.message || 'An unexpected error occurred';
 
-    // console.log(err)
-    // if (err.name === "CastError" && err.typeValue === "ObjectId") {
-    //   message = `Resource Not found:Invalid ObjectId`;
-    //   statusCode = 404;
-    // }
-
-    res.status(statusCode).json({
-      message,
-      stack: process.env.NODE_ENV === "production" ? null : err.stack,})
-
-  }catch(newErr){
-    console.log(newErr);
-    next(newErr)
+  //check for mongoose ObjectId
+  if(err.name=="CastError" && err.kind==="ObjectId"){
+    message=`Resource Not found:Invalid ObjectId`;
+    statusCode=404;
   }
 
-}
+  if(res.headersSent){
+    return next()
+  }
+
+
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,})
+}}
 
 export {notFound,errorHandler}
