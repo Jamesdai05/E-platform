d0a72fd06b80c3bf3a4e66a7f1c93bb666a490e1
jamesdai05 <dai_guilong@hotmail.com>
2025-03-04 15:23:05 +0800

    add the order model

---
+++ backend/models/orderModel.js
@@ -0,0 +1,95 @@
+import mongoose from 'mongoose';
+
+
+const orderModel=mongoose.Schema({
+  user:{
+    type:mongoose.Schema.Types.ObjectId,
+    required:true,
+    ref:"User",
+  },
+  orderItems:[
+    {
+      name:{type:String,required:true},
+      qty:{type:Number,required:true},
+      image:{type:String,required:true},
+      price:{type:Number,required:true},
+      products:{
+        type:mongoose.Schema.Types.ObjectID,
+        required:true,
+        ref:"Product",
+      }
+    }
+  ],
+  shippingAddress:{
+    address:{
+      type:String,
+      required:true,
+    },
+    city:{
+      type:String,
+      required:true,
+    },
+    postcode:{
+      type:String,
+      required:true,
+    },
+    country:{
+      type:String,
+      required:true,
+    }
+  },
+
+  paymentMethod:{
+    type:String,
+    required:true,
+  },
+  paymentResult:{
+    id:{type:String},
+    status:{type:String},
+    update_time:{type:String},
+    email_address:{type:String},
+  },
+  item_price:{
+    type:Number,
+    required:true,
+    default:0.0,
+  },
+  tax_price:{
+    type:Number,
+    required:true,
+    default:0.0,
+  },
+  shipping_price:{
+    type:Number,
+    required:true,
+    default:0.0,
+  },
+
+  total_price:{
+    type:Number,
+    required:true,
+    default:0.0,
+  },
+  isPaid:{
+    type:Boolean,
+    required:true,
+    default:false,
+  },
+  padiAt:{
+    type:Date,
+  },
+  isDelivered:{
+    type:Boolean,
+    required:true,
+    default:false,
+  },
+
+  deliveredAt:{
+    type:Date,
+  }
+
+},{timestamps:true})
+
+
+const Order=mongoose.model('Order',orderSchema);
+export default Order;
