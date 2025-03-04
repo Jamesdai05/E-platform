db879783d42ddd2d44eb308552ae257ea316ad7e
jamesdai05 <dai_guilong@hotmail.com>
2025-03-04 14:10:34 +0800

    setup the datafetch from the backend

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
 import Productlist from "../components/Productlist.jsx";
-import products from "../products";
+// import products from "../products";
+import axios from 'axios';
+
 
 
 const Home = () => {
+
+  const [products,setProducts]=useState([])
+  const productsUrl="api/products"
+
+  useEffect(()=>{
+    const dataFetch = async () => {
+      const { data } = await axios.get(productsUrl);
+      console.log(data);
+      setProducts(data);
+    };
+    dataFetch()
+  },[])
 
   const style = {
     display: "flex",
