6cc58ddcc0ff4e647c1c2e35a7c293dfd01ebc1c
jamesdai05 <dai_guilong@hotmail.com>
2025-03-04 13:47:56 +0800

    config with dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 // console.log("Hello world");
 import express from "express"
 import products from "./data/products.js"
+import dotenv from "dotenv"
+dotenv.config()
 const app=express()
 
-const port =5002
+const port =process.env.PORT || 5002
 
 app.get("/",(req,res)=>{
   res.send("API is running...")
