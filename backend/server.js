27d9d2599e5db14fda4e879d3e5a5e0a0b8037c7
jamesdai05 <dai_guilong@hotmail.com>
2025-03-04 13:13:06 +0800

    completed the server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+// console.log("Hello world");
+import express from "express"
+const app=express()
+
+const port =5002
+
+app.get("/",(req,res)=>{
+  res.send("API is running...")
+})
+
+app.listen(port,()=>{
+  console.log(`Server is listening on port ${port}.`);
+})
