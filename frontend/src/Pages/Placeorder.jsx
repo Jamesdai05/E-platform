e305b90bacb343dd881ab9ddc07be98a387c1cfe
jamesdai05 <dai_guilong@hotmail.com>
2025-03-05 23:41:33 +0800

    setup the basic layout of placeorder page

--- frontend/src/Pages/Placeorder.jsx
+++ frontend/src/Pages/Placeorder.jsx
@@ -1,6 +1,19 @@
+import { Button, Col, Image, ListGroup, Row, Card } from "react-bootstrap";
+import CheckoutComponent from "../components/CheckoutComponent.jsx";
+
+
+
+
+
 const Placeorder = () => {
   return (
-    <div>Placeorder</div>
-  )
-}
-export default Placeorder
+    <>
+      <CheckoutComponent step1 step2 step3 />
+      <Row>
+        <Col md={8}>Col</Col>
+        <Col md={4}>Col</Col>
+      </Row>
+    </>
+  );
+};
+export default Placeorder;
