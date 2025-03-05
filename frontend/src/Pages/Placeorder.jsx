1037485b1eef215c2c6d8c2c14ea4f8bf7375355
jamesdai05 <dai_guilong@hotmail.com>
2025-03-05 23:56:19 +0800

    add the UI of placeorder page

--- frontend/src/Pages/Placeorder.jsx
+++ frontend/src/Pages/Placeorder.jsx
@@ -1,17 +1,120 @@
 import { Button, Col, Image, ListGroup, Row, Card } from "react-bootstrap";
 import CheckoutComponent from "../components/CheckoutComponent.jsx";
-
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from "react-router-dom";
+import Message from "../components/Message.jsx";
 
 
 
 
 const Placeorder = () => {
+
+  const dispatch=useDispatch();
+  const navigate=useNavigate();
+
+  const cart=useSelector(state=>state.cart);
+
+
+
+
   return (
     <>
       <CheckoutComponent step1 step2 step3 />
       <Row>
-        <Col md={8}>Col</Col>
-        <Col md={4}>Col</Col>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>Shipping</h2>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <p>
+                <strong>Address:</strong>
+                <br />
+                <i>
+                  {cart.shippingAddress.address},{cart.shippingAddress.city}
+                </i>
+                <br />
+                <i>
+                  {cart.shippingAddress.postalCode},
+                  {cart.shippingAddress.country}
+                </i>
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <p>
+                <strong>PaymentMethod:</strong>
+                {cart.paymentMethod}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <strong>CartItems:</strong>
+              {cart.cartItems.length === 0 ? (
+                <Message>Your cart is empty.</Message>
+              ) : (
+                <ListGroup>
+                  {cart.cartItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={1}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          />
+                        </Col>
+                        <Col>
+                          <Link
+                            to={`/product/${item._id}`}
+                            className="text-decoration-none hover:text-decoration-underline"
+                          >
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty}X${item.price}=${item.qty * item.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Order Summary:</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>ItemsPrice:</Col>
+                  <Col>${cart.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>TaxPrice:</Col>
+                  <Col>${cart.taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>ShippingPrice:</Col>
+                  <Col>${cart.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>TotalPrice:</Col>
+                  <Col>${cart.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
       </Row>
     </>
   );
