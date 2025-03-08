52a9c9f612dd6933220110d54f90a98cbcd2d265
jamesdai05 <dai_guilong@hotmail.com>
2025-03-08 23:34:24 +0800

    add the edit product UI

--- frontend/src/Pages/Admin/ProductEdit.jsx
+++ frontend/src/Pages/Admin/ProductEdit.jsx
@@ -1,6 +1,61 @@
+import { useEffect, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import Message from "../../components/Message.jsx";
+import Loader from "../../components/Loader.jsx";
+import FormContainer from "../../components/FormContainer.jsx";
+import { toast } from "react-toastify";
+import {
+  useGetProductDetailsQuery,
+  useUpdateProductMutation } from "../../slices/productsSlice.js";
+
+
+
 const ProductEdit = () => {
+  const {id:productId}=useParams();
+
+
+  const [name,setName]=useState("");
+  const [price, setPrice] = useState(0);
+  const [category, setCategory] = useState("");
+  const [image, setImage] = useState("");
+  const [brand, setBrand] = useState("");
+  const [countInStock, setCountInStock] = useState(0);
+  const [description, setDescription] = useState("");
+
+  const {data:product,isLoading,refetch,error}=useGetProductDetailsQuery(productId);
+
+  const [ updateProduct,{isLoading : loadingUpdate}] = useUpdateProductMutation();
+
+  const navigate=useNavigate();
+
+  useEffect(()=>{
+    if(product){
+      setName(product.name);
+      setPrice(product.price);
+      setCategory(product.category);
+      setBrand(product.brand);
+      setCountInStock(product.countInStock);
+      setDescription(product.description);
+    }
+  },[product])
+
+
+  // console.log(product);
+
+
   return (
-    <div>ProductEdit</div>
+    <>
+      <Link to="/admin/productlist" className="btn btn-primary m-3">
+        Go Back
+      </Link>
+
+      <FormContainer>
+
+        
+      </FormContainer>
+
+    </>
   )
 }
 export default ProductEdit
