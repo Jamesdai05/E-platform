880b8d321b30289c0f7ef9ed1b74df264b330df1
jamesdai05 <dai_guilong@hotmail.com>
2025-03-08 23:47:48 +0800

    add the form of editing

--- frontend/src/Pages/Admin/ProductEdit.jsx
+++ frontend/src/Pages/Admin/ProductEdit.jsx
@@ -37,6 +37,7 @@ const ProductEdit = () => {
       setBrand(product.brand);
       setCountInStock(product.countInStock);
       setDescription(product.description);
+      setDescription(product.image);
     }
   },[product])
 
@@ -51,13 +52,97 @@ const ProductEdit = () => {
       </Link>
 
       <FormContainer>
-        <h1>Update Product</h1>
+        <h2>Update Product</h2>
         {loadingUpdate && <Loader />}
 
-        {isLoading ? <Loader /> : error ? <Message variant="danger">{error?.data?.message || error?.error}</Message> : (<h1>Update</h1>)}
-      </FormContainer>
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">
+            {error?.data?.message || error?.error}
+          </Message>
+        ) : (
+          <Form>
+            <Form.Group controlId="name" className="my-2">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter product name"
+                value={name}
+                name="product-name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
 
+            <Form.Group controlId="price" className="my-2">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter product price"
+                value={price}
+                name="price"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="category" className="my-2">
+              <Form.Label>Category</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter the category"
+                value={category}
+                name="category"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="brand" className="my-2">
+              <Form.Label>Brand</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter product brand"
+                value={brand}
+                name="brand"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="countInStock" className="my-2">
+              <Form.Label>Stock</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter the stock"
+                value={stock}
+                name="stock"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="description" className="my-2">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter the description"
+                value={description}
+                name="description"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="image" className="my-2">
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter product name"
+                value={image}
+                name="image"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        )}
+      </FormContainer>
     </>
-  )
+  );
 }
 export default ProductEdit
