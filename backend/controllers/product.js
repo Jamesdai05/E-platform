b972674cc479f7863e1b82b4b51ab31b212078ec
jamesdai05 <dai_guilong@hotmail.com>
2025-03-08 22:14:06 +0800

    add the createProduct controller and its route

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,11 +1,18 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js"
 
+// @desc   fetch all products
+// @route  GET /api/products
+// @access public
 
 const getProducts=asyncHandler(async(req,res)=>{
   const products=await Product.find({})
   res.json(products)
 })
+
+// @desc   fetch a single product
+// @route  GET /api/products/:id
+// @access public
 
 const getProductsById=asyncHandler(async(req,res)=>{
   const product = await Product.findById(req.params.id)
@@ -15,10 +22,31 @@ const getProductsById=asyncHandler(async(req,res)=>{
   res.status(404).json({message:"Product not found."})
 })
 
+// @desc   create a single product
+// @route  POST /api/products
+// @access private/admin
+
+const createProduct=asyncHandler(async(req,res)=>{
+  const product = new Product ({
+    name:"Product",
+    price:0,
+    category:"sample category",
+    user:req.user._id,
+    image:"./images/sample.jpg",
+    brand:"sample brand",
+    category:"sample category",
+    countInStock: 0,
+    numReviews:0,
+    description:"sample description",
+  })
+  const createdProduct =await product.save();
+  res.status(201)).json(createdProduct)
+})
 
 
 
 export {
   getProducts,
   getProductsById,
+  createProduct,
 }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getProducts,getProductsById } from '../controllers/product.js';
+import { createProduct, getProducts,getProductsById } from '../controllers/product.js';
+import { admin, protectRoute } from '../middleware/authMiddleware.js';
+
 
 const router=express.Router()
 
-router.route("/").get(getProducts)
+router.route("/").get(getProducts).post(protectRoute,admin,createProduct)
 router.route("/:id").get(getProductsById)
 
 export default router
