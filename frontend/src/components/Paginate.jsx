31c866e3cbc89341717d886798df661746039835
jamesdai05 <dai_guilong@hotmail.com>
2025-03-11 00:35:19 +0800

    add the size attribute to the component

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -6,17 +6,15 @@ const Paginate = ({pages,page,isAdmin=false}) => {
   // to set the admin for the prodctlist for future development.
   return (
     pages>1 && (
-    <Pagination className="justify-content-center my-4">
+    <Pagination className="justify-content-center my-4" size="sm">
       {[...Array(pages).keys()].map(p=>(
-        // <Link
-        // key={p+1} to={!isAdmin ? `/page/${p+1}` : `admin/productlist/${p+1}` } style={{textDecoration:"none"}}>
 
           <Pagination.Item as={Link}
           key={p+1}
           to={!isAdmin ? `/page/${p+1}` : `admin/productlist/${p+1}` }
           active={p+1===page}
-          aria-label={`go to page ${p+1}`}>{p+1}</Pagination.Item>
-        // </Link>
+          aria-label={`go to page ${p+1}`}
+          >{p+1}</Pagination.Item>
       ))}
     </Pagination>)
   )
